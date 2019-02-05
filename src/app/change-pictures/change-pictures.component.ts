9c0f26a90533d09cde2fdd4db3e68c76b03e3b15
Lily Beaul <lilyb@xello.world>
2019-02-05 13:27:16 -0500

    Added shadow to jumbotron

--- src/app/change-pictures/change-pictures.component.ts
+++ src/app/change-pictures/change-pictures.component.ts
@@ -13,9 +13,13 @@ export class ChangePicturesComponent implements OnInit {
   avatarOptions = ['avatar-1.png', 'avatar-2.png', 'avatar-3.png'];
   coverOptions = ['cover-1.png', 'cover-2.png', 'cover-3.png'];
 
+  currentAvatar = 0;
+  currentCover = 0;
+
   constructor(public profile: ProfileService, private router: Router) { }
 
   ngOnInit() {
+    this.currentAvatar = this.avatarOptions.findIndex(x => this.profile.avatarUrl.indexOf(x) !== -1);
   }
 
   saveChanges() {
@@ -24,10 +28,12 @@ export class ChangePicturesComponent implements OnInit {
 
   changeProfilePicture(option: string) {
     this.profile.avatarUrl = this.baseUrl + option;
+    this.currentAvatar = this.avatarOptions.findIndex(x => this.profile.coverUrl.indexOf(x) !== -1);
   }
 
   changeCoverPicture(option: string) {
     this.profile.coverUrl = this.baseUrl + option;
+    this.currentCover = this.coverOptions.findIndex(x => this.profile.coverUrl.indexOf(x) !== -1);
   }
 
 }
